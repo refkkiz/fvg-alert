a4be400553fc314b7301bf84260159c677963c4a
refik <refik@refik-MacBook-Pro.local>
2026-03-11 11:10:57 +0300

    oanda bos2 yanit fix

--- app.py
+++ app.py
@@ -195,7 +195,7 @@ def scan_loop():
             for pair in data["pairs"]:
                 symbol = pair["symbol"]
                 current_price, fvgs = detect_fvg(symbol)
-                time.sleep(1.5)
+                time.sleep(0.5)
                 if current_price is None:
                     continue
                 pair["last_price"] = current_price
